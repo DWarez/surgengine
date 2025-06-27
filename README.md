696412254f46c3a638ccf960ef792c3c91a55d57
DWarez <ldwarezl@gmail.com>
2025-06-27 14:57:10 +0200

    add: helloworld rest api server

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Roadmap:
 - [ ] Implement basic functional operations using cuBLAS (support cpu only with BLAS?)
 - [ ] CUDA utilities
 - [ ] Implement namespace and modules (equivalent to torch.nn)
+- [ ] Loading safetensors (this sounds painful)
 - [ ] Generation algorithms
 - [ ] KV Cache
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
-#include "kernel.h"
-#include <iostream>
+#include <server/server.hpp>
 
 int main() {
-  std::cout << "Hello from C++!" << std::endl;
-  launch_kernel();
+  std::cout << "Starting server" << std::endl;
+  try {
+    auto const address = net::ip::make_address("0.0.0.0");
+    unsigned short port = 8080;
+
+    net::io_context ioc{1};
+
+    std::cout << "Creating listener on port " << port << std::endl;
+    auto listener =
+        std::make_shared<Listener>(ioc, tcp::endpoint{address, port});
+    listener->run();
+
+    std::cout << "Starting io_context..." << std::endl;
+    ioc.run();
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+
   return 0;
 }
